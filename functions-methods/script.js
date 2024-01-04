a7ceabd84e59a6c745c98fc7f9d7056a0b45a1f9
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-04 20:33:44 +0100

    functions and methods

--- functions-methods/script.js
+++ functions-methods/script.js
@@ -33,3 +33,41 @@
 // const calculatedArea = calcArea(5);
 
 // console.log(calculatedArea);
+
+//arrow function, short and nice but doesnt work with the (this) keyword
+
+// const calcArea = (radius) => {
+//  return 3.14 * radius **2
+// }
+
+//shorter arrow function (only works when you have 1 parameter)
+// const calcArea = (radius) => 3.14 * radius ** 2;
+
+// const area = calcArea(5);
+// console.log(area);
+
+// const greet = () => "hello, world";
+
+// const res = greet()
+// console.log(res);
+
+// const bill = function(products, tax) {
+//     let total = 0
+//     for(let i = 0; i < products.length; i++){
+//         total += products[i] + products[i] * tax
+//     }
+//     return total
+// }
+
+// const bill = (products, tax) => {
+//     let total = 0
+//         for(let i = 0; i < products.length; i++){
+//             total += products[i] + products[i] * tax
+//         }
+//         return total
+// }
+
+// const finalSum = bill([10,15,30],0.2)
+
+
+// console.log(finalSum);
