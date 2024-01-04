9406e6ad9690de97b1bc7ad9248a4973c2278a38
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-04 15:24:31 +0100

    functions and methods

---
+++ functions-methods/script.js
@@ -0,0 +1,35 @@
+//function declaration
+//hoisting
+// function greet(name) {
+//   console.log("hello " + name);
+// }
+
+// //function expression
+// //hoisting doesnt work
+
+// const speak = function() {
+//     console.log("good day");
+// }
+
+// // greet("Deborah")
+// speak()
+
+//arguments and parameters
+
+// const speak = function (name = "Tony", time = "night") {
+//   console.log(`Good ${time} ${name}`);
+// };
+
+// speak("Michael", "morning");
+
+//returning values
+
+// const calcArea = function (radius) {
+//   return 3.14 * radius ** 2;
+
+//   // return area
+// };
+
+// const calculatedArea = calcArea(5);
+
+// console.log(calculatedArea);
