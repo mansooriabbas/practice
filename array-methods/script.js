f00238f5234ab057cedd2c4a3138b0e86b1b0b48
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-08 20:25:23 +0100

    array-methods

--- array-methods/script.js
+++ array-methods/script.js
@@ -31,7 +31,7 @@
 
 // console.log(premiumUsers);
 
-const prices = [25, 150, 300, 75, 500, 80, 250, 120, 400, 90];
+// const prices = [25, 150, 300, 75, 500, 80, 250, 120, 400, 90];
 
 // const salePrices = prices.map((price) => {
 //     return price / 2
@@ -39,20 +39,143 @@ const prices = [25, 150, 300, 75, 500, 80, 250, 120, 400, 90];
 
 // console.log(salePrices);
 
+// const products = [
+//     { name: "Chair", price: 50 },
+//     { name: "Table", price: 20 },
+//     { name: "Lamp", price: 30 },
+//     { name: "Bookshelf", price: 80 },
+//     { name: "Mirror", price: 40 }
+//   ];
+
+//   const saleProducts = products.map((product) => {
+//     if(product.price > 30){
+//         return {name: product.name, price: product.price / 2}
+//     }else{
+//         return product
+//     }
+//   })
+
+//   console.log(saleProducts, products);
+
+// const scores = [25, 10, 30, 75, 50, 8, 20, 120, 40, 90];
+
+// const result = scores.reduce((acc, curr) => {
+//   if(curr > 50){
+//     acc++
+//   }
+//   return acc
+// }, 0)
+
+// console.log(result);
+
+// const scores = [
+//   {
+//     player: "Emma",
+//     score: 20,
+//   },
+//   {
+//     player: "Charles",
+//     score: 65,
+//   },
+//   {
+//     player: "Tony",
+//     score: 50,
+//   },
+//   {
+//     player: "Tony",
+//     score: 25,
+//   },
+// ];
+
+// const tonyTotal = scores.reduce((acc, curr) => {
+// if(curr.player === "Tony"){
+//   acc += curr.score
+// }
+// return acc
+// },0)
+// console.log(tonyTotal);
+
+// const scores = [25, 10, 30, 75, 50, 8, 20, 120, 40, 90];
+
+// const findScore = scores.find((score) => {
+//   // if(score > 50){
+//   //   return score
+//   // }
+
+//   return score > 50
+// })
+
+// console.log(findScore);
+
+// const names = ["Charlie", "Steve", "Tom", "Brandy"];
+
+// names.sort() // this method is destructive and changes the array, so you cant create a new variable
+// names.reverse()
+// console.log(names);
+
+// const scores = [30, 10, 20, 60, 70, 5];
+// scores.sort((a, b) => {
+//   return b - a
+// })
+// scores.reverse()
+// console.log(scores);
+
+// const players = [
+//   {
+//     player: "Emma",
+//     score: 20,
+//   },
+//   {
+//     player: "Charles",
+//     score: 65,
+//   },
+//   {
+//     player: "Tony",
+//     score: 50,
+//   },
+//   {
+//     player: "Tony",
+//     score: 25,
+//   },
+// ];
+
+// players.sort((a, b) => {
+//   if (a.score > b.score) {
+//     return -1;
+//   } else if (b.score > a.score) {
+//     return 1;
+//   }else{
+//     return 0
+//   }
+// });
+
+// players.sort((a, b) => {
+//   return b.score - a.score
+// })
+
+// console.log(players);
+
 const products = [
-    { name: "Chair", price: 50 },
-    { name: "Table", price: 20 },
-    { name: "Lamp", price: 30 },
-    { name: "Bookshelf", price: 80 },
-    { name: "Mirror", price: 40 }
-  ];
+  {
+    name: "chair",
+    price: 20,
+  },
+  {
+    name: "lamp",
+    price: 65,
+  },
+  {
+    name: "desk",
+    price: 50,
+  },
+  {
+    name: "sofa",
+    price: 25,
+  },
+];
+//method chaining
+const promos = products
+.filter(product => product.price > 20)
+.map(product => `The ${product.name} is ${product.price / 2} pounds`)
 
-  const saleProducts = products.map((product) => {
-    if(product.price > 30){
-        return {name: product.name, price: product.price / 2}
-    }else{
-        return product
-    }
-  })
-
-  console.log(saleProducts, products);
+console.log(promos);
