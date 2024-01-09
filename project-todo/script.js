2da61a257e5a74a06f2e1c7bf01d8d46ff9c9bb9
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-09 13:22:28 +0100

    project-todo

--- project-todo/script.js
+++ project-todo/script.js
@@ -1,5 +1,6 @@
 const addForm = document.querySelector(".add");
 const list = document.querySelector(".todos");
+const search = document.querySelector(".search input");
 
 const generateTemplate = (todo) => {
   const html = `
@@ -18,5 +19,35 @@ addForm.addEventListener("submit", (e) => {
 
   if (todo.length) {
     generateTemplate(todo);
+    addForm.reset();
   }
+});
+
+list.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    e.target.parentElement.remove();
+  }
+});
+
+const filterTodos = (term) => {
+  Array.from(list.children)
+    .filter((todo) => {
+      return !todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      return todo.classList.add("filtered");
+    });
+
+  Array.from(list.children)
+    .filter((todo) => {
+      return todo.textContent.toLowerCase().includes(term);
+    })
+    .forEach((todo) => {
+      return todo.classList.remove("filtered");
+    });
+};
+
+search.addEventListener("keyup", (e) => {
+  const term = search.value.trim().toLowerCase();
+  filterTodos(term);
 });
