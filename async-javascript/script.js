d1029a6bb00cc5f1f939c55ec2293684179d019f
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-14 23:51:00 +0100

    section completed

--- async-javascript/script.js
+++ async-javascript/script.js
@@ -29,8 +29,31 @@
 //     console.log("promise rejected", err);
 //   });
 
-
 // fetch api
 
+const getTodos = async () => {
+  const response = await fetch("./todos.json");
 
+  if(response.status !== 200){
+    throw new Error("cannot fetch the data")
+  }
+  const data = await response.json();
 
+  return data;
+};
+
+getTodos().then((data) => {
+  console.log("resolved", data);
+}).catch((err) =>{
+console.log("rejected:", err.message);
+})
+// fetch("./todos.json").then((response) => {
+// console.log("resolved", response);
+// return response.json()
+// }).then((data) =>{
+// console.log(data);
+// }).catch((err) =>{
+// console.log("reject", err);
+// })
+
+//012 async await
