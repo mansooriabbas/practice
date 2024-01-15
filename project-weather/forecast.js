d9c82ccb1d75b46e6047076567a9d174e6ba402b
mansooriabbas <mansoori.abbas@outlook.com>
2024-01-15 15:24:25 +0100

    section completed

---
+++ project-weather/forecast.js
@@ -0,0 +1,27 @@
+const key = "	lRE6lOA2b4U4PS4eGw5Rba0CfBkIsI8p";
+const id = "314929";
+//get weather information
+const getWeather = async (id) => {
+  
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
+
+// get city information
+const getCity = async (city) => {
+
+  const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+
+};
